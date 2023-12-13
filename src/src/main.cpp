211ac3f5b416f6ecc92e84a906537c5b37c6d46f
= <2566374h@student.gla.ac.uk>
2023-12-13 00:36:11 +0000

    Added basic SD functionality, ability to toggle display

--- src/src/main.cpp
+++ src/src/main.cpp
@@ -1,5 +1,7 @@
 #include <Arduino.h>
 #include <Wire.h>
+#include <SPI.h>
+#include <SD.h>
 #include <U8g2lib.h>
 #include <INA226_WE.h>
 #include <INA3221.h>
@@ -8,6 +10,7 @@
 U8G2 *u8g2 = new U8G2_SSD1306_128X64_NONAME_F_HW_I2C(U8G2_R0, /* reset=*/ U8X8_PIN_NONE);
 INA226_WE ina226 = INA226_WE(0x40);
 INA3221 ina3221(INA3221_ADDR41_VCC);
+File log_file;
 
 bool serial_mode = false;
 float busVoltage_V = 0.0;
@@ -20,55 +23,96 @@ unsigned long last_avg = 0;
 float v_iter = 0;
 float mA_iter = 0;
 int iter_counter = 0;
+int log_counter = 0;
 
 float SoC;
 // On startup, look up estimated SoC based on current voltage, assuming current is (close to) zero.
 // After initial SoC has been measured begin coloumb counting.
 
 void setup() {
-  u8g2->begin();
-  u8g2->clearBuffer();
-
-  if (serial_timeout > 0) {
-    u8g2->setFont(u8g2_font_profont17_mr);
-    u8g2->setCursor(0, 49);
-    u8g2->print("Connecting...");
-    u8g2->sendBuffer();
-
-    Serial.begin(115200);
-    long start_time = millis();
-    while ((millis() - start_time) < (serial_timeout * 1000)) {
-      if (Serial.readString() == "hello") {
-        serial_mode = true;
-        break;
+  if (sd_logging == 1) {
+    if (!SD.begin(15)) {
+      while(1){};
+    } else {
+      while (SD.exists(String(log_counter) + ".csv")) {
+        log_counter++;
       }
-    };
+      log_file = SD.open((String(log_counter) + ".csv"), FILE_WRITE);
+
+      if (log_file) {
+        log_file.println("BMS Logging");
+        log_file.close();
+      }
+    }
   }
 
-  if (serial_mode) {
-    Serial.println("BMS");
+  if (use_display == 1) {
+    u8g2->begin();
     u8g2->clearBuffer();
-    u8g2->setCursor(0, 49);
-    u8g2->print("Connected!");
-    u8g2->sendBuffer();
-    delay(1000);
-    u8g2->clearBuffer();
-    u8g2->sendBuffer();
+
+    if (serial_timeout > 0) {
+      u8g2->setFont(u8g2_font_profont17_mr);
+      u8g2->setCursor(0, 49);
+      u8g2->print("Connecting...");
+      u8g2->sendBuffer();
+
+      Serial.begin(115200);
+      long start_time = millis();
+      while ((millis() - start_time) < (serial_timeout * 1000)) {
+        if (Serial.readString() == "hello") {
+          serial_mode = true;
+          break;
+        }
+      };
+    }
+
+    if (serial_mode) {
+      Serial.println("BMS");
+      u8g2->clearBuffer();
+      u8g2->setCursor(0, 49);
+      u8g2->print("Connected!");
+      u8g2->sendBuffer();
+      delay(1000);
+      u8g2->clearBuffer();
+      u8g2->sendBuffer();
+    } else {
+      u8g2->setFont(u8g2_font_inr46_mf);
+      u8g2->setCursor(0, 49);
+      u8g2->print("VLT");
+      u8g2->sendBuffer();
+      delay(500);
+    }
+
+    if(!ina226.init()) {
+      u8g2->clearBuffer();
+      u8g2->setFont(u8g2_font_profont17_mr);
+      u8g2->setCursor(0, 49);
+      u8g2->print("INA226 INIT FAIL");
+      u8g2->sendBuffer();
+      while(1){}
+    }
   } else {
-    u8g2->setFont(u8g2_font_inr46_mf);
-    u8g2->setCursor(0, 49);
-    u8g2->print("VLT");
-    u8g2->sendBuffer();
-    delay(500);
-  }
+    // seemingly required to run in all cases
+    u8g2->begin();
 
-  if(!ina226.init()) {
-    u8g2->clearBuffer();
-    u8g2->setFont(u8g2_font_profont17_mr);
-    u8g2->setCursor(0, 49);
-    u8g2->print("INA226 INIT FAIL");
-    u8g2->sendBuffer();
-    while(1){}
+    if (serial_timeout > 0) {
+      Serial.begin(115200);
+      long start_time = millis();
+      while ((millis() - start_time) < (serial_timeout * 1000)) {
+        if (Serial.readString() == "hello") {
+          serial_mode = true;
+          break;
+        }
+      };
+    }
+
+    if (serial_mode) {
+      Serial.println("BMS");
+    }
+
+    if(!ina226.init()) {
+      while(1){}
+    }
   }
 
   ina226.setResistorRange(0.00041229385, 200.0);
@@ -101,6 +145,32 @@ void loop() {
     mA_iter += current_mA;
     iter_counter ++;
   }
+
+  if (sd_logging == 1) {
+    log_file = SD.open((String(log_counter) + ".csv"), FILE_WRITE);
+
+    if (log_file) {
+      // Time
+      log_file.print(millis());
+      log_file.print(",");
+      // V1
+      log_file.print(voltage[0]);
+      log_file.print(",");
+      // V2
+      log_file.print(voltage[1] - voltage[0]);
+      log_file.print(",");
+      // V3
+      log_file.print(voltage[2] - voltage[1]);
+      log_file.print(",");
+      // V4
+      log_file.print(busVoltage_V - voltage[2]);
+      log_file.print(",");
+      // Current
+      log_file.print(current_mA);
+      log_file.println(",");
+      log_file.close();
+    }
+  }
   
   if (serial_mode) {
     Serial.print("v1,");
@@ -111,7 +181,7 @@ void loop() {
     Serial.println(voltage[2] - voltage[1]);
     Serial.print("v4,");
     Serial.println(busVoltage_V - voltage[2]);
-  } else {
+  } else if (use_display == 1) {
     u8g2->clearBuffer();
     u8g2->setFont(u8g2_font_profont17_mr);
     u8g2->setCursor(0, u8g2->getMaxCharHeight());
