191cea80065ad9c894f50b86b390a3dd4f3541af
Karl Hartmann <2566374h@student.gla.ac.uk>
2024-01-21 15:00:46 +0000

    Continued work on resistance estimation implementation

--- src/lib/ResistanceEstimate/resistance_estimate.cpp
+++ src/lib/ResistanceEstimate/resistance_estimate.cpp
@@ -4,7 +4,7 @@
 
 void ResistanceEstimate::update_resistance_estimate(float current_amps, float voltage, unsigned long now) {
     // return immediately if no current
-    if (current_amps <= 0) {
+    if (!is_positive(current_amps)) {
         return;
     }
 
@@ -27,22 +27,36 @@ void ResistanceEstimate::update_resistance_estimate(float current_amps, float vo
 
     // estimate short-term resistance
     float filt_alpha = constrain_float(loop_interval/(loop_interval + AP_BATT_MONITOR_RES_EST_TC_1), 0.0f, 0.5f);    // constrain_float(val, min, max) returns val limited to min/ max
-    float resistance_alpha = MIN(1, AP_BATT_MONITOR_RES_EST_TC_2*fabsf((_state.current_amps-_current_filt_amps)/_current_max_amps));
-    float resistance_estimate = -(_state.voltage-_voltage_filt)/current_delta;
+    float resistance_alpha = 1 < AP_BATT_MONITOR_RES_EST_TC_2*fabsf((current_amps-current_filt_amps_)/current_max_amps_) ? 1 : AP_BATT_MONITOR_RES_EST_TC_2*fabsf((current_amps-current_filt_amps_)/current_max_amps_);
+    float resistance_estimate = -(voltage-voltage_filt_)/current_delta;
     if (is_positive(resistance_estimate)) {
-        _state.resistance = _state.resistance*(1-resistance_alpha) + resistance_estimate*resistance_alpha;
+        resistance = resistance*(1-resistance_alpha) + resistance_estimate*resistance_alpha;
     }
 
     // calculate maximum resistance
-    if ((_resistance_voltage_ref > _state.voltage) && (_state.current_amps > _resistance_current_ref)) {
-        float resistance_max = (_resistance_voltage_ref - _state.voltage) / (_state.current_amps - _resistance_current_ref);
-        _state.resistance = MIN(_state.resistance, resistance_max);
+    if ((resistance_voltage_ref_ > voltage) && (current_amps > resistance_current_ref_)) {
+        float resistance_max = (resistance_voltage_ref_ - voltage) / (current_amps - resistance_current_ref_);
+        resistance = resistance < resistance_max ? resistance : resistance_max;
     }
 
     // update the filtered voltage and currents
-    _voltage_filt = _voltage_filt*(1-filt_alpha) + _state.voltage*filt_alpha;
-    _current_filt_amps = _current_filt_amps*(1-filt_alpha) + _state.current_amps*filt_alpha;
+    voltage_filt_ = voltage_filt_*(1-filt_alpha) + voltage*filt_alpha;
+    current_filt_amps_ = current_filt_amps_*(1-filt_alpha) + current_amps*filt_alpha;
 
     // update estimated voltage without sag
-    _state.voltage_resting_estimate = _state.voltage + _state.current_amps * _state.resistance;
+    voltage_resting_estimate_ = voltage + current_amps * resistance;
+}
+
+float constrain_float(float val, float min, float max) {
+    if (val > max) {
+        return max;
+    } else if (val < min) {
+        return min;
+    } else {
+        return val;
+    }
+}
+
+bool is_positive(float val) {
+    return (val >= __FLT_EPSILON__);
 }
--- src/lib/ResistanceEstimate/resistance_estimate.h
+++ src/lib/ResistanceEstimate/resistance_estimate.h
@@ -1,3 +1,9 @@
+#include <cmath>
+
+// Time constants for filters
+#define AP_BATT_MONITOR_RES_EST_TC_1        0.5f
+#define AP_BATT_MONITOR_RES_EST_TC_2        0.1f
+
 class ResistanceEstimate {
     private:
         float filtered_current_;
@@ -5,11 +11,14 @@ class ResistanceEstimate {
         int reaction_time_;
         float current_max_amps_;
         float current_filt_amps_;
+        float voltage_filt_;
         float resistance_voltage_ref_;
         float resistance_current_ref_;
         unsigned long resistance_timer_ms_;
 
     public:
+        float resistance;
+        float voltage_resting_estimate_;
         ResistanceEstimate();
-        update_resistance_estimate();
+        void update_resistance_estimate(float current_amps, float voltage, unsigned long now);
 };
