7efe10c2885070c9f15ee4248d54bcf6fb3973ee
Karl Hartmann <2566374h@student.gla.ac.uk>
2024-01-29 19:49:42 +0000

    Update define to create array

--- src/src/defines.h
+++ src/src/defines.h
@@ -13,4 +13,4 @@
 // Measure voltages over a very gradual discharge to build a table of (almost) open circuit voltages.
 // From these results, build lookup table, mapping percentage of capacity to voltage.
 // Next create function to interpolate if between percentage values.
-#define percent_at_voltage [[100, 4.2], [99, 4.1]]
+const float mah_at_voltage[] = {0.0, 1550.0};
