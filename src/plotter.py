fefd00631917c054cf7ca8dc0a300c030f97e1be
= <2566374h@student.gla.ac.uk>
2024-01-30 14:44:12 +0000

    Created function to define lookup array for battery voltages to mAh used

--- src/plotter.py
+++ src/plotter.py
@@ -65,6 +65,62 @@ def low_pass_filter_array(array, value_index, time_index, filter_const):
     for index in range(len(array) - 1):
         array[index + 1][value_index] = lpf.update(float(array[index + 1][value_index]), int(array[index + 1][time_index]) / 1000000)
 
+def find_empty_and_fill(array, end=True):
+    if end:
+        start_counter = len(array) - 1
+        end_counter = len(array)
+
+        value = array[start_counter]
+
+        while value == 0:
+            start_counter -= 1
+            value = array[start_counter]
+        
+        array[start_counter + 1: end_counter] = value
+    
+    else:
+        start_counter = 0
+        end_counter = 0
+
+        value = array[0]
+
+        while value == 0:
+            end_counter += 1
+            value = array[end_counter]
+        
+        array[start_counter: end_counter + 1] = value
+
+
+def get_lookup_vals(array, values_array, max_voltage, min_voltage):
+    output = np.zeros(int(max_voltage * 100) - int(min_voltage * 100))
+    prev_val = array[0]
+    min_reached = False
+    max_reached = False
+
+    for index, value in enumerate(array):
+        if value > max_voltage:
+            max_reached = True
+            continue
+        if value < min_voltage:
+            min_reached = True
+            continue
+
+        if output[int(value * 100) - int(min_voltage * 100)] == 0:
+            output[int(value * 100) - int(min_voltage * 100)] = values_array[index]
+
+            if int(prev_val * 100) - int(value * 100) > 1:
+                output[int(value * 100) - int(min_voltage * 100) + 1: int(prev_val * 100) - int(min_voltage * 100)] = values_array[index]
+                
+            prev_val = value
+
+    if not max_reached:
+        find_empty_and_fill(output)
+        
+    if not min_reached:
+        find_empty_and_fill(output, False)
+
+    return output
+
 
 with open(r"C:\Users\Karl\dev\predictive-bms\data\raw\test-20240123-212326.csv") as F:
     lines = [line.strip().split(',') for line in F.readlines()]
@@ -81,15 +137,23 @@ results_copy = average_results(results_copy, 4, 1000)
 # print(results_copy[0])
 # print(results_copy[-1])
 
-gradients = [0]
+print(get_lookup_vals(results_copy[::, 4].astype(float), results_copy[::, 7].astype(float), 16.8, 13.3))
 
-for index in range(len(results_copy) - 1):
-    gradients.append(float(results_copy[index + 1][4]) - float(results_copy[index][4]))
+# gradients = [0]
+# for index in range(len(results_copy) - 1):
+#     gradients.append(float(results_copy[index + 1][4]) - float(results_copy[index][4]))
 
-plt.title("Discharge Test 1\n1500mAh 4s, Low Pass Filter with 1/300 constant, Averaging every 1000 values")
-plt.xlabel("Capacity (mAh)")
-plt.ylabel("Voltage (V)")
-plt.grid(True)
-# plt.plot(results_copy[::, 7].astype(float), results_copy[::, 4].astype(float))
-plt.plot(results_copy[::, 7].astype(float), gradients)
-plt.show()
+# fig, ax1 = plt.subplots()
+# ax1.set_xlabel("Capacity (mAh)")
+# ax1.set_ylabel("Voltage (V)", color='tab:red')
+# ax1.plot(results_copy[::, 7].astype(float), results_copy[::, 4].astype(float), color='tab:red')
+# ax1.tick_params(axis='y', labelcolor='tab:red')
+
+# ax2 = ax1.twinx()
+# ax2.set_ylabel("V Difference (V)", color='tab:blue')
+# ax2.plot(results_copy[::, 7].astype(float), gradients, color='tab:blue')
+# ax2.tick_params(axis='y', labelcolor='tab:blue')
+
+# plt.title("Discharge Test 1\n1500mAh 4s, Low Pass Filter with 1/300 constant, Averaging every 1000 values")
+# plt.grid(True)
+# plt.show()
