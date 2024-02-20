fb7b1fea7ec24908efb2164c48548ca8fb440090
Karl Hartmann <2566374h@student.gla.ac.uk>
2024-02-20 09:17:06 +0000

    Comment out interrupt code, stabilisation code for data gathering, add logging of bus voltage and shunt voltage

--- src/src/main.cpp
+++ src/src/main.cpp
@@ -20,6 +20,7 @@ BattMonitor *monitor;
 
 bool serial_mode = false;
 float busVoltage_V = 0.0;
+float shuntVoltage_mV = 0.0;
 float cell_voltages[4];
 float current_mA = 0.0;
 float mAh_charged = 0;
@@ -124,7 +125,7 @@ void setup() {
     ina226.setConversionTime(CONV_TIME_2116);
     ina226.setAverage(AVERAGE_16);
     ina226.enableConvReadyAlert();
-    pinMode(INTERRUPT_PIN, INPUT_PULLUP);
+    // pinMode(INTERRUPT_PIN, INPUT_PULLUP);
     // Total time = 2.116ms conversion * 2 readings * 16 averages = 67.712ms
     ina3221.begin();
     ina3221.reset();
@@ -135,34 +136,35 @@ void setup() {
 
     unsigned long zero_amp_start = millis();
     cur_time = millis();
-    while ((millis() - zero_amp_start) < STABILISATION_TIME_MS) {
-        delay(0);   // Required to allow for ESP background processes to run
-        if (USE_DISPLAY) {
-            u8g2->clearBuffer();
-            u8g2->setCursor(0, u8g2->getMaxCharHeight());
-            u8g2->print("Stabilising");
-            u8g2->setCursor(0, u8g2->getMaxCharHeight() * 2);
-            u8g2->printf("Time left:%lu", STABILISATION_TIME_MS - (millis() - zero_amp_start));
-            u8g2->setCursor(0, u8g2->getMaxCharHeight() * 3);
-            u8g2->printf("mA:%.0f", shuntVoltageTomA(ina226.getShuntVoltage_mV()));
-            u8g2->sendBuffer();
-        }
-        if (ina226.isBusy()) {
-            ina226.waitUntilConversionCompleted();
-        }
-        if ((shuntVoltageTomA(ina226.getShuntVoltage_mV()) > ZERO_AMP_CUTOFF) || (shuntVoltageTomA(ina226.getShuntVoltage_mV()) < (-ZERO_AMP_CUTOFF))) {
-            zero_amp_start = millis();
-        }
-        loop_time = millis() - cur_time;
-        cur_time = millis();
-    }
+    // while ((millis() - zero_amp_start) < STABILISATION_TIME_MS) {
+    //     delay(0);   // Required to allow for ESP background processes to run
+    //     if (USE_DISPLAY) {
+    //         u8g2->clearBuffer();
+    //         u8g2->setCursor(0, u8g2->getMaxCharHeight());
+    //         u8g2->print("Stabilising");
+    //         u8g2->setCursor(0, u8g2->getMaxCharHeight() * 2);
+    //         u8g2->printf("Time left:%lu", STABILISATION_TIME_MS - (millis() - zero_amp_start));
+    //         u8g2->setCursor(0, u8g2->getMaxCharHeight() * 3);
+    //         u8g2->printf("mA:%.0f", shuntVoltageTomA(ina226.getShuntVoltage_mV()));
+    //         u8g2->sendBuffer();
+    //     }
+    //     if (ina226.isBusy()) {
+    //         ina226.waitUntilConversionCompleted();
+    //     }
+    //     if ((shuntVoltageTomA(ina226.getShuntVoltage_mV()) > ZERO_AMP_CUTOFF) || (shuntVoltageTomA(ina226.getShuntVoltage_mV()) < (-ZERO_AMP_CUTOFF))) {
+    //         zero_amp_start = millis();
+    //     }
+    //     loop_time = millis() - cur_time;
+    //     cur_time = millis();
+    // }
 
     // Initialise battery model/ monitor
     simulator = new BattModel(MAX_VOLTAGE, CAPACITY, NOMINAL_VOLTAGE, NOMINAL_CAPACITY, INTERNAL_RESISTANCE, EXPONENTIAL_VOLTAGE, EXPONENTIAL_CAPACITY, CURVE_CURRENT, MIN_VOLTAGE);
     modifiable_simulator = new BattModel(MAX_VOLTAGE, CAPACITY, NOMINAL_VOLTAGE, NOMINAL_CAPACITY, INTERNAL_RESISTANCE, EXPONENTIAL_VOLTAGE, EXPONENTIAL_CAPACITY, CURVE_CURRENT, MIN_VOLTAGE);
 
     busVoltage_V = ina226.getBusVoltage_V();
-    current_mA = shuntVoltageTomA(ina226.getShuntVoltage_mV());
+    shuntVoltage_mV = ina226.getShuntVoltage_mV();
+    current_mA = shuntVoltageTomA(shuntVoltage_mV);
     cell_voltages[0] = ina3221.getVoltage(INA3221_CH1);
     cell_voltages[1] = ina3221.getVoltage(INA3221_CH2) -  ina3221.getVoltage(INA3221_CH1);
     cell_voltages[2] = ina3221.getVoltage(INA3221_CH3) - ina3221.getVoltage(INA3221_CH2);
@@ -178,7 +180,7 @@ void loop() {
     cur_time = micros();
     // Ensures new reading on every loop, block until available if not yet available.
     // INA226 checked first, as its conversion time is greater
-    detachInterrupt(digitalPinToInterrupt(INTERRUPT_PIN));
+    // detachInterrupt(digitalPinToInterrupt(INTERRUPT_PIN));
     if (ina226.isBusy()) {
         ina226.waitUntilConversionCompleted();
     } else {
@@ -193,30 +195,31 @@ void loop() {
         }
     }
 
-    if (missed_count) {
-        busVoltage_V = (ina226.getBusVoltage_V() + v_iter) / (missed_count + 1);
-        // Consider also getting missed INA3221 values if possible
-        cell_voltages[0] = ina3221.getVoltage(INA3221_CH1);
-        cell_voltages[1] = ina3221.getVoltage(INA3221_CH2) -  ina3221.getVoltage(INA3221_CH1);
-        cell_voltages[2] = ina3221.getVoltage(INA3221_CH3) - ina3221.getVoltage(INA3221_CH2);
-        cell_voltages[3] = busVoltage_V - ina3221.getVoltage(INA3221_CH3);
-        current_mA = shuntVoltageTomA((ina226.getShuntVoltage_mV() + shunt_mV_iter) / (missed_count + 1));
+    // if (missed_count) {
+    //     busVoltage_V = (ina226.getBusVoltage_V() + v_iter) / (missed_count + 1);
+    //     // Consider also getting missed INA3221 values if possible
+    //     cell_voltages[0] = ina3221.getVoltage(INA3221_CH1);
+    //     cell_voltages[1] = ina3221.getVoltage(INA3221_CH2) -  ina3221.getVoltage(INA3221_CH1);
+    //     cell_voltages[2] = ina3221.getVoltage(INA3221_CH3) - ina3221.getVoltage(INA3221_CH2);
+    //     cell_voltages[3] = busVoltage_V - ina3221.getVoltage(INA3221_CH3);
+    //     current_mA = shuntVoltageTomA((ina226.getShuntVoltage_mV() + shunt_mV_iter) / (missed_count + 1));
         
-        missed_count = 0;
-        shunt_mV_iter = 0;
-        v_iter = 0;
-    } else {
-        busVoltage_V = ina226.getBusVoltage_V();
-        cell_voltages[0] = ina3221.getVoltage(INA3221_CH1);
-        cell_voltages[1] = ina3221.getVoltage(INA3221_CH2) -  ina3221.getVoltage(INA3221_CH1);
-        cell_voltages[2] = ina3221.getVoltage(INA3221_CH3) - ina3221.getVoltage(INA3221_CH2);
-        cell_voltages[3] = busVoltage_V - ina3221.getVoltage(INA3221_CH3);
-        current_mA = shuntVoltageTomA(ina226.getShuntVoltage_mV());
-    }
+    //     missed_count = 0;
+    //     shunt_mV_iter = 0;
+    //     v_iter = 0;
+    // } else {
+    busVoltage_V = ina226.getBusVoltage_V();
+    cell_voltages[0] = ina3221.getVoltage(INA3221_CH1);
+    cell_voltages[1] = ina3221.getVoltage(INA3221_CH2) -  ina3221.getVoltage(INA3221_CH1);
+    cell_voltages[2] = ina3221.getVoltage(INA3221_CH3) - ina3221.getVoltage(INA3221_CH2);
+    cell_voltages[3] = busVoltage_V - ina3221.getVoltage(INA3221_CH3);
+    shuntVoltage_mV = ina226.getShuntVoltage_mV();
+    current_mA = shuntVoltageTomA(shuntVoltage_mV);
+    // }
 
 
 
-    attachInterrupt(digitalPinToInterrupt(INTERRUPT_PIN), measureNow, FALLING);
+    // attachInterrupt(digitalPinToInterrupt(INTERRUPT_PIN), measureNow, FALLING);
     // delay(1000); // to test interrupt
 
     // monitor->updateConsumption(micros(), 60, busVoltage_V, current_mA, cell_voltages);
@@ -227,6 +230,9 @@ void loop() {
         if (log_file) {
             // Time
             log_file.print(millis());
+            log_file.print(",");
+            // Voltage
+            log_file.print(busVoltage_V);
             log_file.print(",");
             // V1
             log_file.print(cell_voltages[0]);
@@ -248,6 +254,9 @@ void loop() {
             log_file.print(",");
             // mWh Used
             log_file.print(mWh_charged);
+            log_file.print(",");
+            // Shunt Voltage Used
+            log_file.print(shuntVoltage_mV);
             log_file.println(",");
             log_file.close();
         } else {
